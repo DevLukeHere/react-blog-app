38bd79bd47ba3019655d24a3cb26e98326415bba
Luke Wong <luke@wobbjobs.com>
2021-01-02 21:51:22 +0800

    Add Static Blog Posts

--- react-blog/src/Home.js
+++ react-blog/src/Home.js
@@ -1,7 +1,20 @@
+import { useState } from 'react';
+
 const Home = () => {
+  const [blogs, setBlogs] = useState([
+    { title:"Hello World!", body:"Blah...blah...blah...", author: "Batman", id: 1},
+    { title:"Happy New Year 2021!", body:"Blah...blah...blah...", author: "Wonder Woman", id: 2},
+    { title:"React Blogs are the Best!", body:"Blah...blah...blah...", author: "Superman", id: 3}
+  ]);
+
   return ( 
     <div className="home">
-      <h2>Homepage</h2>
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{ blog.title }</h2>
+          <p>Written by: { blog.author }</p>
+        </div>
+      ))}
     </div>
   );
 }
