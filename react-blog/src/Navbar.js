c50b67cc980f1ad3fcb27eb1b32ff5223385adf2
Luke Wong <luke@wobbjobs.com>
2021-01-02 21:18:30 +0800

    Add Styling to Homepage

--- react-blog/src/Navbar.js
+++ react-blog/src/Navbar.js
@@ -1,10 +1,16 @@
 const Navbar = () => {
+  const newBlogStyle = {
+    color: '#ffff',
+    backgroundColor: '#f1356d',
+    borderRadius: '8px',
+  }
+
   return ( 
     <nav className="navbar">
       <h1>Simple React Blog Page</h1>
       <div className="links">
         <a href="/">Home</a>
-        <a href="/create">New Blog</a>
+        <a href="/create" style={newBlogStyle}>New Blog</a>
       </div>
     </nav>
   );
