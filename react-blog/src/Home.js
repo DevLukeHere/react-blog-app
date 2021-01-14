b0fdae7fdb67563834341880a66506b98adbb268
Luke Wong <luke@wobbjobs.com>
2021-01-14 21:39:02 +0800

    Refactor Fetch Data using Custom Hooks

--- react-blog/src/Home.js
+++ react-blog/src/Home.js
@@ -1,35 +1,14 @@
-import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
+import useFetch from './useFetch';
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null);
-  const [isPending, setIsPending] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    fetch('http://localhost:8000/blogs')
-      .then(respond => {
-        if (!respond.ok) {
-          throw Error("Failed to fetch data from resource!");
-        }
-        return respond.json()
-      })
-      .then(data => {
-        setBlogs(data);
-        setIsPending(false);
-        setError(null);
-      })
-      .catch(err => {
-        setIsPending(false);
-        setError(err.message);
-      })
-  }, []);
+  const { data, isPending, error } = useFetch('http://localhost:8000/blogs')
 
   return ( 
     <div className="home">
       { error && <div>{ error }</div> }
       { isPending && <div>Loading blogs...</div> }
-      { blogs && <BlogList blogs={blogs} title="All Blogs!" /> } 
+      { data && <BlogList blogs={data} title="All Blogs!" /> } 
     </div>
   );
 }
---
+++ react-blog/src/useFetch.js
@@ -0,0 +1,30 @@
+import { useState, useEffect } from 'react';
+
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch(url)
+      .then(respond => {
+        if (!respond.ok) {
+          throw Error("Failed to fetch data from resource!");
+        }
+        return respond.json()
+      })
+      .then(data => {
+        setData(data);
+        setIsPending(false);
+        setError(null);
+      })
+      .catch(err => {
+        setIsPending(false);
+        setError(err.message);
+      })
+  }, [url]);
+
+  return { data, isPending, error }
+}
+
+export default useFetch
