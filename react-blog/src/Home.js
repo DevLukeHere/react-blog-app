d1b286b040f996afc0756d3f44229924f9ea7573
Luke Wong <luke@wobbjobs.com>
2021-01-14 21:27:16 +0800

    Handle Error Messages

--- react-blog/src/Home.js
+++ react-blog/src/Home.js
@@ -4,20 +4,30 @@ import BlogList from './BlogList';
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
   const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     fetch('http://localhost:8000/blogs')
       .then(respond => {
+        if (!respond.ok) {
+          throw Error("Failed to fetch data from resource!");
+        }
         return respond.json()
       })
       .then(data => {
         setBlogs(data);
         setIsPending(false);
+        setError(null);
+      })
+      .catch(err => {
+        setIsPending(false);
+        setError(err.message);
       })
   }, []);
 
   return ( 
     <div className="home">
+      { error && <div>{ error }</div> }
       { isPending && <div>Loading blogs...</div> }
       { blogs && <BlogList blogs={blogs} title="All Blogs!" /> } 
     </div>
