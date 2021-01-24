de2b0e29a72eb912593f71519e3572303e7a5e72
Luke Wong <luke@wobbjobs.com>
2021-01-24 17:58:55 +0800

    Handle 404 Errors

---
+++ react-blog/src/404Page.js
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom';
+
+const ErrorPage = () => {
+  return (
+    <div className="not-found">
+      <h2>Sorry</h2>
+      <p>404 Page Not Found!</p>
+      <Link to='/'>Back to Homepage ></Link>
+    </div>
+  );
+}
+ 
+export default ErrorPage;
